3597c3f3e1c95313ca2d0f96617b0f849dee7709
Hedie098 <Hedie>
2024-03-07 23:46:10 -0600

    Got a basic outline running on a server

---
+++ antage-rag/src/page/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container w-[1751px] h-[1638px] relative mx-auto my-0'>
+      <div className='w-[102px] h-[84px] bg-[url(../assets/images/7188b99c0198aaf424ac0fcc807454f2bc10a627.png)] bg-cover bg-no-repeat relative z-10 mt-[16px] mr-0 mb-0 ml-[13px]' />
+      <div className='w-[303px] h-[292px] bg-[url(../assets/images/8d693e62c09fff0dc595db54b7b6aa8f044eeea4.png)] bg-cover bg-no-repeat rounded-[39px] relative z-[9] mt-[18px] mr-0 mb-0 ml-[713px]' />
+      <span className="block h-[53px] font-['B612_Mono'] text-[36px] font-normal leading-[43.74px] text-[#fff] relative text-left whitespace-nowrap z-[6] mt-[13px] mr-0 mb-0 ml-[683px]">
+        Tuesday,Feb 27
+      </span>
+      <div className='w-[1344.003px] h-[5.116px] bg-[url(../assets/images/69edfdd2-adeb-4fb3-90eb-60a625e364fb.png)] bg-[length:100%_100%] bg-no-repeat relative z-[3] mt-[12px] mr-0 mb-0 ml-[208px]' />
+      <span className="flex w-[1284px] h-[244px] justify-start items-start font-['B612_Mono'] text-[30px] font-bold leading-[36.45px] text-[#fff] relative text-left z-[14] mt-[43.884px] mr-0 mb-0 ml-[211px]">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat."
+      </span>
+      <span className="block h-[42px] font-['B612_Mono'] text-[30px] font-bold leading-[36.45px] text-[#fff] relative text-left whitespace-nowrap z-[4] mt-[17px] mr-0 mb-0 ml-[204px]">
+        ChatGPT:
+      </span>
+      <span className="flex w-[1344px] h-[195px] justify-start items-start font-['B612_Mono'] text-[30px] font-bold leading-[36.45px] text-[#fff] relative text-left z-[5] mt-0 mr-0 mb-0 ml-[204px]">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat."
+      </span>
+      <div className='flex w-[127px] pt-[10px] pr-[10px] pb-[10px] pl-[10px] gap-[10px] justify-center items-center flex-nowrap relative z-[11] mt-[496px] mr-0 mb-0 ml-[204px]'>
+        <button className='flex w-[138px] pt-[10px] pr-[10px] pb-[10px] pl-[10px] gap-[10px] justify-center items-center shrink-0 flex-nowrap bg-[#cdd4df] rounded-[20px] border-none relative z-[12] pointer'>
+          <span className="h-[36px] shrink-0 basis-auto font-['B612_Mono'] text-[30px] font-bold leading-[36px] text-[#1a1717] relative text-left whitespace-nowrap z-[13]">
+            Upload
+          </span>
+        </button>
+      </div>
+      <div className='w-full h-full bg-[#1e293c] border-solid border border-[#000] absolute top-0 left-0' />
+      <div className='w-[76.76%] h-[24.54%] bg-[#3e3b3b] rounded-[15px] border-solid border-2 border-[#fdf3f3] absolute top-[68.93%] left-[11.65%] box-content z-[1]' />
+      <span className="flex h-[2.2%] justify-start items-start font-['B612_Mono'] text-[30px] font-bold leading-[36px] text-[#fff] absolute top-[70.27%] left-[12.85%] text-left whitespace-nowrap z-[2]">
+        This is some sample text that the user will input.
+      </span>
+      <div className='w-[7.25%] h-[2.93%] bg-[#ccd3df] rounded-[30px] absolute top-[88.58%] left-[79.78%] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] z-[7]' />
+      <div className='w-[2.34%] h-[2.01%] bg-[url(../assets/images/e71b4f01-4635-4661-9d36-57a9c5945a85.png)] bg-[length:100%_100%] bg-no-repeat absolute top-[89.01%] left-[82.24%] z-[8]' />
+    </div>
+  );
+}
