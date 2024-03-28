71b27e9d457cf347258e40092083fc457c686e48
Uriel6u <122196184+Uriel6u@users.noreply.github.com>
2024-03-28 02:27:06 -0500

    fixed div layouts for more simplicity, adjust sizing of tailwind util, implement layout with two sections 'you' and 'ai assistant'

--- antage-rag/src/app/page.tsx
+++ antage-rag/src/app/page.tsx
@@ -61,42 +61,45 @@ export default function Home() {
       });
   };
   return (
-    <div className='main-container max-w-screen mx-auto flex flex-col items-center bg-[rgba(30,41,60,1)] '>
+    <div className='main-container min-h-screen flex flex-col items-center bg-[rgba(30,41,60,1)]'>
       {/*Antage logo*/}
-      <div className='w-24 h-20 bg-[url(../../assets/images/Antagelogo.png)] bg-cover bg-no-repeat absolute top-2 left-2 ' />
+      <div className='w-24 h-20 bg-[url(../../assets/images/Antagelogo.png)] bg-cover bg-no-repeat absolute top-3 left-2 ' />
       {/*openai image*/}
       <div className='w-64 h-64 bg-[url(../../assets/images/OpenAI.png)] bg-cover bg-no-repeat rounded-lg mt-8' />
       {/*date*/}
       <span className="block mt-4 text-xl text-white">
         {formattedDate} {/* Display the formatted date here */}
       </span>
-      
-      {/* chat history of user */}
-      <div className="w-full max-w-sreen-lg mt-4">
-        <span className="block mt-4 text-lg font-bold text-white">User:</span>
-        <div className="bg-#486391 rounded-lg border border-gray-300 p-4 text-white">
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat."
-        </div>
-      </div>
 
-       {/* Chat response */}
-       <div className="w-full max-w-sreen-lg mt-4">
-        <span className="block mt-4 text-lg font-bold text-white">ai Assistant:</span>
-        <div className="bg-#486391 rounded-lg border border-gray-300 p-4 text-white">
-        {responseData &&
-          Object.entries(responseData).map(([key, value]) => (
-            <div key={key}>
-              <strong>{key}:</strong> {value}
-            </div>
-          ))}
+
+      <div className="w-full max-w-screen-lg mt-4 flex flex-col items-center">
+        {/* chat history of user */}
+        <div className="w-full max-w-sreen-lg mt-4">
+          <span className="block mt-4 text-lg font-bold text-white">User:</span>
+          <div className="bg-[#0c4a6e] rounded-lg border border-gray-300 p-4 text-white w-full">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eleifend non leo cursus mollis. Nulla sit amet eros tincidunt libero venenatis vulputate tempor quis velit. Nam mi metus,
+           tempus eget neque in, blandit ornare augue. Morbi lobortis, augue id faucibus volutpat, felis magna eleifend eros, mattis egestas velit lorem id ante. Nulla a pretium leo, a finibus nibh.
+            Quisque consectetur rutrum mi, quis cursus nulla vehicula varius. Nullam libero odio, rutrum nec mollis vehicula, eleifend id mi. Suspendisse lacus nibh, sagittis sit amet lorem id, venenatis"
+           
+          </div>
+        </div>
+
+        {/* Chat history of ai response */}
+        <div className="w-full max-w-sreen-lg mt-4">
+          <span className="block mt-4 text-lg font-bold text-white">ai Assistant:</span>
+          <div className="bg-[#0c4a6e] rounded-lg border border-gray-300 p-4 text-white w-full">
+          {responseData &&
+            Object.entries(responseData).map(([key, value]) => (
+              <div key={key}>
+                <strong>{key}:</strong> {value}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-     
       {/* chatbox */}
-      <div className="w-full max-w-screen-lg bg-[#403c3c] rounded-lg border mt-7 relative">
+      <div className="w-full max-w-screen-lg bg-[#403c3c] rounded-lg border border-gray-300 fixed bottom-12">
+
         <textarea
           value={userInput}
           onChange={(e) => setUserInput(e.target.value)}
@@ -106,13 +109,13 @@ export default function Home() {
 
         <div
           onClick={handleSend}
-          className="flex justify-center items-center w-16 h-16 bg-[#ccd3df] rounded-full absolute bottom-4 right-4 cursor-pointer"
+          className="flex justify-center items-center w-16 h-9 bg-[#ccd3df] rounded-full absolute bottom-4 right-4 cursor-pointer"
           style={{ cursor: "pointer" }}
         >
           <img
             src="../../assets/images/sendButton.png"
             alt="Send Image"
-            className='w-[60%] h-[70%] rounded-[30px]'
+            className='w-[60%] h-[70%] '
           />
         </div>
       </div>
